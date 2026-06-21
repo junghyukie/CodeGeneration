e9aecb91cd7b48d610dfc14e10025571b4f0ec47
ankhanhtran02 <ankhanhtran02@gmail.com>
2026-06-21 06:45:44 -0400

    +

--- scripts/router_codetask.sh
+++ scripts/router_codetask.sh
@@ -6,7 +6,7 @@ export CUDA_VISIBLE_DEVICES
 
 python gmm.py \
   --model_name Qwen/Qwen2.5-Coder-1.5B \
-  --output_dir router/router_gmm_codetask_vf0.0001_dim_256_comp_4_layer_4 \
+  --output_dir router/router_gmm_codetask_vf0.02_dim_256_comp_4_layer_4 \
   --dataset_source codetask \
   --tasks CONCODE,CodeTrans,CodeSearchNet,BFP,KodCode,RunBugRun,TheVault_Csharp,CoST \
   --batch_size 16 \
--- scripts/router_executable.sh
+++ scripts/router_executable.sh
@@ -1,19 +1,18 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-CUDA_VISIBLE_DEVICES=0
+CUDA_VISIBLE_DEVICES=1
 export CUDA_VISIBLE_DEVICES
 
 python gmm.py \
   --model_name      Qwen/Qwen2.5-Coder-1.5B \
-  --output_dir      ./router_exe/router_gmm_exe_vf0.02_dim_256_comp_4_layer_4 \
+  --output_dir      ./router/router_gmm_exe_vf0.02_dim_256_comp_4_layer_4 \
   --dataset_source  executable \
   --tasks           python,cpp,swift,rust,csharp,java,php,typescript,shell \
   --batch_size      16 \
   --train_k         5000 \
   --eval_k          -1 \ 
   --variance_floor  0.02 \
-  --eval_k          1000 \
   --routing_dim     256 \
   --gmm_components  4 \
   --feature_layers  4 \
